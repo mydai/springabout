a3c4d406d4421d0c72d8d86030582830306a32ec
cuidaidai <cuizhixiangwow@qq.com>
2018-01-23 16:25:12 +0800

    新建项目server-zipkin

--- springCloud/config-client/src/main/java/com/daidai/start/ConfigClientApplication.java
+++ springCloud/config-client/src/main/java/com/daidai/start/ConfigClientApplication.java
@@ -3,11 +3,14 @@ package com.daidai.start;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+//刷新标志，添加后刷新才有效
+@RefreshScope
 public class ConfigClientApplication {
 
     public static void main(String[] args) {
